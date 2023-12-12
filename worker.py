33bad34e92291597775607cdc1e1fcc8c34c0dac
joshuaflores <floresjoshua2603@gmail.com>
2023-12-12 23:53:16 +0000

    Changes to worker.py

--- worker.py
+++ worker.py
@@ -3,16 +3,27 @@ from flask import request
 import requests
 import os
 import json
+from google.cloud import secretmanager_v1
 app = Flask(__name__)
 
 def get_api_key() -> str:
-    secret = os.environ.get("COMPUTE_API_KEY")
-    if secret:
-        return secret
-    else:
-        #local testing
-        with open('.key') as f:
-            return f.read()
+    # secret = os.environ.get("COMPUTE_API_KEY")
+    project_id = "warm-rookery-400321"
+    secret_id = "compute-api-key"
+    
+    client = secretmanager_v1.SecretManagerServiceClient()
+    
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    
+    return response.payload.data.decode("UTF-8")
+  
+    # if secret:
+    #     return secret
+    # else:
+    #     #local testing
+    #     with open('.key') as f:
+    #         return f.read()
       
 @app.route("/")
 def hello():
@@ -38,7 +49,7 @@ def addWorker(token, num):
       tdata=json.load(p)
     tdata['name']='slave'+str(num)
     data=json.dumps(tdata)
-    url='https://www.googleapis.com/compute/v1/projects/spark-371009/zones/europe-west1-b/instances'
+    url='https://www.googleapis.com/compute/v1/projects/warm-rookery-400321/zones/europe-west1-b/instances'
     headers={"Authorization": "Bearer "+token}
     resp=requests.post(url,headers=headers, data=data)
     if resp.status_code==200:     
