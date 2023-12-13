1fe4bc6d87917c2eca9725b3aecd007b9cefb054
joshuaflores <floresjoshua2603@gmail.com>
2023-12-13 13:56:24 +0000

    Modify worker.py

--- worker.py
+++ worker.py
@@ -8,25 +8,14 @@ from google.cloud import secretmanager
 app = Flask(__name__)
 
 def get_api_key() -> str:
-    # secret = os.environ.get("compute-api-key")
-    # 
-    # if secret:
-    #     return secret
-    # else:
-    #     #local testing
-    #     with open('.key') as f:
-    #         return f.read()
+    secret = os.environ.get("compute-api-key")
     
-    client = secretmanager.SecretManagerServiceClient()
-
-    # Build the resource name of the secret version.
-    name = f"projects/635007151197/secrets/compute-api-key/versions/2"
-    
-    # Access the secret version.
-    response = client.access_secret_version(name=name)
-
-    # Return the decoded payload.
-    return "ya29.a0AfB_byBGXcBEJAXisSdA7_iqiVJYSTtEOI_GDJrs6umtsEXxXQg7MFacsKgy-SGUzTj5O208hZgA3Loqjb7DerDrfK9ZBLynzQuYGlN98Nl-YE3laTMsV7dNJtgUSLXqPTf1-S2m0NQkhsm-hxUO_Fdy04LDzNrSq2Sb7fE2IL4Pc2VDhgZcewhjQYgqQIBH0F0L2Zf9UDXcyp4OGcvwOMOWVv0_SrZgwEOOIXqlUH26VzAHnIVJfBSNIrLia9xo3qkYg8g-Z5Vjnqbn8KyzrYdsOx17Nd9Dk-o3z0qtB68YBj2LuJhhotQPn4wxWV5sf1DhfwR7ai6QvfPyy0XKOnTe626FvlKN8zpHTQFwiHyn5xDS4TfYvLW-3eOrWRLLbI3xJ911GuMl0yWim_TACJveV0hMArEaCgYKAS4SARASFQHGX2MiUNWETZuzoIIc2HYdGdFMSQ0422"
+    if secret:
+        return secret
+    else:
+        #local testing
+        with open('.key') as f:
+            return f.read()
       
 @app.route("/")
 def hello():
@@ -42,7 +31,7 @@ def add():
   if request.method=='GET':
     return "Use post to add" # replace with form template
   else:
-    token= get_api_key()
+    token= "ya29.a0AfB_byBGXcBEJAXisSdA7_iqiVJYSTtEOI_GDJrs6umtsEXxXQg7MFacsKgy-SGUzTj5O208hZgA3Loqjb7DerDrfK9ZBLynzQuYGlN98Nl-YE3laTMsV7dNJtgUSLXqPTf1-S2m0NQkhsm-hxUO_Fdy04LDzNrSq2Sb7fE2IL4Pc2VDhgZcewhjQYgqQIBH0F0L2Zf9UDXcyp4OGcvwOMOWVv0_SrZgwEOOIXqlUH26VzAHnIVJfBSNIrLia9xo3qkYg8g-Z5Vjnqbn8KyzrYdsOx17Nd9Dk-o3z0qtB68YBj2LuJhhotQPn4wxWV5sf1DhfwR7ai6QvfPyy0XKOnTe626FvlKN8zpHTQFwiHyn5xDS4TfYvLW-3eOrWRLLbI3xJ911GuMl0yWim_TACJveV0hMArEaCgYKAS4SARASFQHGX2MiUNWETZuzoIIc2HYdGdFMSQ0422"
     ret = addWorker(token,request.form['num'])
     return ret
 
