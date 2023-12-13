5b034f8707f7ce619a035897d51015a4bdad0400
joshuaflores <floresjoshua2603@gmail.com>
2023-12-13 14:22:51 +0000

    .

--- worker.py
+++ worker.py
@@ -5,7 +5,6 @@ import os
 import json
 from google.cloud import secretmanager_v1
 app = Flask(__name__)
-
 def get_api_key() -> str:
     secret = os.environ.get("COMPUTE_API_KEY")
     # project_id = "635007151197"
@@ -28,21 +27,19 @@ def get_api_key() -> str:
 @app.route("/")
 def hello():
     return "Add workers to the Spark cluster with a POST request to add"
-
 @app.route("/test")
 def test():
     #return "Test" # testing 
     return(get_api_key())
-
 @app.route("/add",methods=['GET','POST'])
 def add():
   if request.method=='GET':
     return "Use post to add" # replace with form template
   else:
+    token=get_api_key()
     token= "ya29.a0AfB_byCL9rKOkkmtAGD4aLKrKaYILQtBl_rPvZLShJFTjPmVtmr14t08dzOaGWRtHSuI5cdso4WaHemICPrjoZEkZB-0f7gf5O5clGPCaSJlZ3Ginplw3AoBD5f3TKUcWycvYI1252xgERkNUYeLNdwuijQusxOAQJ4rKOKgTEWvbigY1r25Odv_9gKNEDoFzWhsoMXmY4TW0gQLV8Ybvy5SVA40sGgLCuCkZHMHoN0r3mcPa38y9GdoqssLscZWypPsc7ahcXN0MffKePlApw4zG4sAlGERyb-wr4rWoq07WSkNWzYQOiI6quNMphDwcw_A31mGxNns3tiUusiFAOs0LP4YrJTYylcDSebb_HzKMq6pSdL_rRFsRV9bdkwt1vnUdPBKAPt968p39p7HVW22eLjGmm8aCgYKAegSARASFQHGX2MigE6ITL7DQBbsSImiDiaogg0422"
     ret = addWorker(token,request.form['num'])
     return ret
-
 
 def addWorker(token, num):
     with open('payload.json') as p:
@@ -57,8 +54,5 @@ def addWorker(token, num):
     else:
       print(resp.content)
       return "Error\n"+resp.content.decode('utf-8') + '\n\n\n'+data
-
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='8080')
